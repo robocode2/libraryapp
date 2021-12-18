0760e98c2536befb9b913f7cde8b11ee09137fd8
robocode2 <rabie.abbas@code.berlin>
2021-12-18 21:28:08 +0100

    added category routes

---
+++ server/routes/categoryRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const category_controller = require('../controllers/categoryController');
+const router = express.Router();
+
+// POST request for creating Category.
+router.post('/category/create', category_controller.category_create_post);
+
+// POST request to delete Category.
+router.delete('/category/:id', category_controller.category_delete_post);
+
+// POST request to update Category.
+router.put('/category/:id', category_controller.category_update_post);
+
+// GET request for one Category.
+router.get('/category/:id', category_controller.category_detail);
+
+// GET request for list of all Category items.
+router.get('/', category_controller.category_list);
+
+module.exports = router;
