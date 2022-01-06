37f169f20227923007778ae6fd7222bde94e0798
robocode2 <rabie.abbas@code.berlin>
2022-01-06 12:37:02 +0100

    remove ssl from db confing

--- server/config/database/config/config.js
+++ server/config/database/config/config.js
@@ -15,9 +15,9 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
-    dialectOptions: { ssl: true },
-    logging: false,
+    //use_env_variable: 'DATABASE_URL',
+    //dialectOptions: { ssl: true },
+    //logging: false,
   },
 };
 
