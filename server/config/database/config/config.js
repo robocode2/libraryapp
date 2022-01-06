da1c162213e7f4c5768af403b46bf8f13e762312
robocode2 <rabie.abbas@code.berlin>
2022-01-06 09:41:51 +0100

    set SSL true for production environment

--- server/config/database/config/config.js
+++ server/config/database/config/config.js
@@ -15,6 +15,9 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: { ssl: true },
+    logging: false,
   },
 };
 
