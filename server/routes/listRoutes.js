05f306a6b0f92c1af6fbba667b7f260ece1378b0
robocode2 <rabie.abbas@code.berlin>
2022-01-17 13:46:09 +0100

    created List API endpoints

--- server/routes/listRoutes.js
+++ server/routes/listRoutes.js
@@ -3,20 +3,29 @@ const list_controller = require('../controllers/listController');
 const router = express.Router();
 
 // POST request for creating list.
-router.post('/list/create', list_controller.list_create_post);
+router.post('/lists/create', list_controller.list_create);
 
 // POST request to delete list.
-router.delete('/list/:id', list_controller.list_delete_post);
+router.delete('/lists/:name', list_controller.list_delete);
 
 // POST request to update list.
-router.put('/list/:id', list_controller.list_update_post);
+router.put('/lists/:name', list_controller.list_update);
 
 // GET request for one list.
-router.get('/list/:id', list_controller.list_detail);
+router.get('/lists/:name', list_controller.list_details);
 
 // GET request for list of all list items.
 router.get('/lists', list_controller.lists_list);
 
+// GET request for list of all list items for a particular user. FUTURE YSE
+//router.get('/lists/?user_uid={user_uid}', list_controller.userlists_list);
+//router.get('/lists', list_controller.userlists_list);
+
+// GET request for list of user;s books in a particular list.
+//router.get('/lists/:listname', list_controller.user_list);
+
 //Add book to list
+
+// BLA BLA BLA HERE PLEASE IMPLEMENT ADDING BOOK To LIST
 
 module.exports = router;
