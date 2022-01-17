69a1f86440eb03f8ad7cbccf1c352bbda6680997
robocode2 <rabie.abbas@code.berlin>
2022-01-17 13:42:05 +0100

    implementing getTokenID in list_controller

--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -3,22 +3,19 @@ const admin = require('../config/firebase/firebase-config');
 
 const middleware = require('../middleware/index');
 
-// Display list of all categories.
-exports.lists_list = async (req, res) => {
-  try {
-    const lists = await List.findAll();
-    return res.json(lists);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
-  }
-};
+// CALL CONSEQUENCE METHODS FROM ENTRIES_CONTROLLER
 
-// Handle list create on POST.
-exports.list_create_post = async (req, res) => {
+// Handle list create .
+exports.list_create = async (req, res) => {
+  const user = req['currentUser'];
+  console.log('good morning honey' + user.uid);
+  if (!user) {
+    res.status(403).send('You must be logged in!');
+  }
   try {
-    const userid = await middleware.getJWTToken(req, res);
     const { name, description } = req.body;
+    //const userid = await middleware.getUserId(req, res);
+    const userid = user.uid;
     const date1 = new Date();
     const date2 = new Date();
     const list = await List.create({ userid, name, description, date1, date2 });
@@ -29,30 +26,13 @@ exports.list_create_post = async (req, res) => {
   }
 };
 
-// Display details for a specific list.
-exports.list_detail = async (req, res) => {
-  const id = req.params.id; //name?
-  console.log('hi good morning ');
-  try {
-    const list = await List.findOne({
-      where: { id },
-    });
-
-    return res.json(list);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
-  }
-};
-
 // Handle list delete on POST.
-exports.list_delete_post = async (req, res) => {
+exports.list_delete = async (req, res) => {
+  // delete list entries from User Booklist Entries
   const id = req.params.id;
   try {
     const list = await List.findOne({ where: { id } });
-
     await list.destroy(); //onCascade? all books that contain this tag ?
-
     return res.json({ message: 'list deleted!' });
   } catch (err) {
     console.log(err);
@@ -61,18 +41,55 @@ exports.list_delete_post = async (req, res) => {
 };
 
 // Handle list update on POST.
-exports.list_update_post = async (req, res) => {
-  //is id ok? revert to id/uuid?
-
+exports.list_update = async (req, res) => {
   const { name, description } = req.body;
   try {
     const list = await List.findOne({ where: { id } });
-
     list.name = name;
     list.description = description;
-
     await List.save();
+    return res.json(list);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
 
+// Display list of all user's lists.
+exports.lists_list = async (req, res) => {
+  const user = req['currentUser'];
+  if (!user) {
+    res.status(403).send('You must be logged in!');
+  }
+  try {
+    const lists = await List.findAll({
+      where: { userid: user.uid },
+    });
+    return res.json(lists);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+// Display entries in user's specific list.
+exports.list_details = async (req, res) => {
+  const user = req['currentUser'];
+  console.log('good morning honey' + user);
+  if (!user) {
+    res.status(403).send('You must be logged in!');
+  }
+  //name?
+  const list_name = req.params.list_name; //name?
+  //LIST DETAILS
+  try {
+    const list = await List.findOne({
+      where: { userid: user.uid, name: list_name },
+    });
+    // ADD :
+    // GRAB ALL BOOK ENTRIES WITH THAT LIST ID FROM THE 3RD DATABASE TABLE
+    // COMBINE THEM INTO OBJECT AND RETURN
+    // OR CREATE EXTRA ENDPOINT /lists/:listname/books
     return res.json(list);
   } catch (err) {
     console.log(err);
