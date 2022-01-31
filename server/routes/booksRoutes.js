69e47aabbb58942479c0db8ce6dae9fd5afd0c8c
robocode2 <rabie.abbas@code.berlin>
2022-01-31 12:49:14 +0100

    plural route names

--- server/routes/booksRoutes.js
+++ server/routes/booksRoutes.js
@@ -9,22 +9,22 @@ const router = express.Router();
 //router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
-router.post('/book/create', book_controller.book_create_post);
+router.post('/books/create', book_controller.book_create_post);
 
 // GET request to delete Book.
 //router.get('/book/:id/delete', book_controller.book_delete_get);
 
 // POST request to delete Book.
-router.delete('/book/:id', book_controller.book_delete_post);
+router.delete('/books/:id', book_controller.book_delete_post);
 
 // GET request to update Book.
 //router.get('/book/:id/update', book_controller.book_update_get);
 
 // POST request to update Book.
-router.put('/book/:id', book_controller.book_update_post);
+router.put('/books/:id', book_controller.book_update_post);
 
 // GET request for one Book.
-router.get('/book/:id', book_controller.book_detail);
+router.get('/books/:id', book_controller.book_detail);
 
 // GET request for list of all Book items.
 router.get('/books', book_controller.book_list);
